262a249a2476d6406d68d54deb959d92a5a4acb8
Anandhan <anandhanedom@gmail.com>
2021-01-15 12:17:45 +0530

    how to solve

---
+++ 002_howToSolve/01_interview/app.js
@@ -0,0 +1,15 @@
+// Given 2 arrays, create a function that
+// let's user know (true/false) whether these 2 arrays contain any common items
+// For example :
+// const array1 = ['a', 'b', 'c', 'x'];
+// const array2 = ['z', 'y', 'i'];
+//should return false
+// ___________________________________
+// const array1 = ['a', 'b', 'c', 'x'];
+// const array2 = ['z', 'y', 'x'];
+//should return true
+
+//***** ANSWER + APPROACH *****
+
+//2 params - arrays
+//return true/false
