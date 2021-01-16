eb6b214f52e75335abc0d17ea752dc9ec060e594
Anandhan <anandhanedom@gmail.com>
2021-01-16 11:03:42 +0530

    sort

--- 003_arrays/04_stringRev1/app.js
+++ 003_arrays/04_stringRev1/app.js
@@ -2,7 +2,6 @@
 
 function reverse(str) {
   //Check length
-
   if (!str || str.length < 2 || typeof str !== 'string') {
     return 'Hmm..that is not good';
   }
@@ -16,4 +15,20 @@ function reverse(str) {
   return revArray.join('');
 }
 
+function reverse2(str) {
+  //Check length
+  if (!str || str.length < 2 || typeof str !== 'string') {
+    return 'Hmm..that is not good';
+  }
+
+  return str.split('').reverse().join('');
+}
+
+const reverse3 = (str) => str.split('').reverse().join('');
+
+const reverse4 = (str) => [...str].reverse().join('');
+
 console.log(reverse('Malayalam'));
+console.log(reverse2('Hilltop'));
+console.log(reverse3('Zack'));
+console.log(reverse4('Mountain'));
---
+++ 003_arrays/05_mergeSortedArr/app.js
@@ -0,0 +1,8 @@
+const arr1 = [0, 3, 4, 31];
+const arr2 = [4, 6, 30];
+
+const mergeSortedArrays = (arr1, arr2) => {
+  const mergedArray = [];
+
+  return mergedArray;
+};
