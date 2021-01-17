85391251c10a9399036777ac434f61e9cdc520b3
Anandhan <anandhanedom@gmail.com>
2021-01-18 00:32:37 +0530

    sort arr

--- 003_arrays/05_mergeSortedArr/app.js
+++ 003_arrays/05_mergeSortedArr/app.js
@@ -1,6 +1,3 @@
-const arr1 = [0, 3, 4, 31];
-const arr2 = [4, 6, 30];
-
 const mergeSortedArrays = (array1, array2) => {
   const mergedArray = [];
 
@@ -11,8 +8,8 @@ const mergeSortedArrays = (array1, array2) => {
     j = 1;
 
   //Check the inputs
-  if (Array.isArray(array1) && Array.isArray(array2)) {
-    return 'Hmm....check the inputs!';
+  if (!Array.isArray(array1) || !Array.isArray(array2)) {
+    return 'Hmm....check the inputs!!';
   } else if (array1.length === 0) {
     return array2;
   } else if (array2.length === 0) {
@@ -20,12 +17,23 @@ const mergeSortedArrays = (array1, array2) => {
   }
 
   while (array1Item || array2Item) {
-    if (array1Item < array2Item) {
+    // console.log(array1Item, array2Item);
+
+    if (!array2Item || array1Item < array2Item) {
       mergedArray.push(array1Item);
+      array1Item = array1[i];
+      i++;
     } else {
       mergedArray.push(array2Item);
+      array2Item = array2[j];
+      j++;
     }
   }
 
   return mergedArray;
 };
+
+const arr1 = [0, 3, 4, 31];
+const arr2 = [4, 6, 30];
+
+console.log(mergeSortedArrays([0, 3, 4, 31], [4, 6, 30]));
